5b6caf2d108f0e189edfa2128a9a835f2241ce38
Simon <simibac93@gmail.com>
2022-04-16 20:43:08 +0200

    package path added

--- Package.swift
+++ Package.swift
@@ -26,9 +26,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConfettiSwiftUI",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "ConfettiSwiftUITests",
-            dependencies: ["ConfettiSwiftUI"]),
+            dependencies: ["ConfettiSwiftUI"],
+            path: "Tests"),
+        
     ]
 )
