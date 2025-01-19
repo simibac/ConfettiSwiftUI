ffc46c1b1e208a4e93224ddf5db49ff5da0659d3
Volodymyr Boichentsov <sakristx@gmail.com>
2025-01-19 15:26:05 +0000

    change to import

--- Sources/ConfettiSwiftUI.swift
+++ Sources/ConfettiSwiftUI.swift
@@ -139,7 +139,7 @@ public struct ConfettiCannon<T: Equatable>: View {
                 for i in 0..<confettiConfig.repetitions{
                     DispatchQueue.main.asyncAfter(deadline: .now() + confettiConfig.repetitionInterval * Double(i)) {
                         animate.append(false)
-                        #if os(iOS)
+#if canImport(UIKit)
                         if confettiConfig.hapticFeedback {
                             let impactFeedback = UIImpactFeedbackGenerator(style: .heavy)
                             impactFeedback.impactOccurred()
@@ -149,7 +149,7 @@ public struct ConfettiCannon<T: Equatable>: View {
                             let impactFeedback = UIImpactFeedbackGenerator(style: .heavy)
                             impactFeedback.impactOccurred()
                         }
-                        #endif
+#endif
                     }
                 }
             }
